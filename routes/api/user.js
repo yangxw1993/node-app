6a4499dd5a7d52e3c33770fdb6b1bcc38aeb2faa
杨小伟 <2389469307@qq.com>
2021-03-27 15:12:39 +0800

    update: 用户登录

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-03-01 23:19:15
- * @LastEditTime: 2021-03-21 22:59:32
+ * @LastEditTime: 2021-03-27 15:11:54
  * @LastEditors: Please set LastEditors
  * @FilePath: /node-app/routes/api/user.js
  */
 const express = require('express');
+const gravatar = require('gravatar'); 
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../../models/User');
@@ -13,9 +14,8 @@ const User = require('../../models/User');
 router.get('/test', (req,res) => {
   res.json({msg: 'login works'})
 });
-
+// 注册 
 router.post('/register', (req, res) => {
-  console.log(req.body  );
   const {email, name, password} = req.body;
   User.findOne({
     email
@@ -26,8 +26,9 @@ router.post('/register', (req, res) => {
         msg: '邮箱已注册'
       })
     }else{
+      const avatar = gravatar.url(email, {s: '200', r: 'pg', d: 'mm'})
       const newUser = new User({
-        name, email, password, avatar: ''
+        name, email, password, avatar
       });
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(password, salt, (err, hash) => {
@@ -41,6 +42,23 @@ router.post('/register', (req, res) => {
     }
   })
   // res.send(newUser)
+})
+
+// 登录
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  // 查询
+  User.findOne({email}).then(user => {
+    if(!user){
+      return res.json({code: 1, msg: '用户不存在!'})
+    }
+    console.log(user);
+
+    // 密码匹配
+    const isMatch = bcrypt.compareSync(password, user.password);
+    return res.json({ code: isMatch ? 0 : 1, msg: isMatch ? 'ok' : '用户名或密码错误' })
+  })
 })
 
  
